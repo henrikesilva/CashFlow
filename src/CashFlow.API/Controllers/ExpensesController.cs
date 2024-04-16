9555c7f9c32aac73b156fe2618c6088588610278
Henrique Lima <henrikelima.0502@gmail.com>
2024-04-15 21:22:11 -0300

    Partial completion of the first module
    Implemented exception filters
    
    Validations implemented for request classes;
    
    Creation middleware and resources files to switch language

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,8 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CashFlow.API.Controllers;
 
@@ -13,25 +11,10 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        try
-        {
-            var useCase = new RegisterExpenseUseCase();
+        var useCase = new RegisterExpenseUseCase();
 
-            var response = useCase.Execute(request);
+        var response = useCase.Execute(request);
 
-            return Created(string.Empty, response);
-        }
-        catch(ArgumentException ex) 
-        {
-            var errorResponse = new ResponseErrorJson(ex.Message);
-
-            return BadRequest(errorResponse);
-        }
-        catch
-        {
-            var errorResponse = new ResponseErrorJson("unknown error");
-
-            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-        }
+        return Created(string.Empty, response);
     }
 }
