7677ba637b37a0d47652b604c1bcfda05d2aac8a
Henrique Lima <henrikelima.0502@gmail.com>
2024-07-19 22:56:41 -0300

    Created expense update test
    - Created unit test to Update Expense Use Case
    - Cretaed integration test to Update Expense
    - Created new Expense Mock repository to update expense tests
    - Created new Http call to Class fixture
    - Refactoring Request Expense Json name

--- tests/CommomTestUtilities/Requests/RequestExpenseJsonBuilder.cs
+++ tests/CommomTestUtilities/Requests/RequestExpenseJsonBuilder.cs
@@ -3,7 +3,7 @@ using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 
 namespace CommomTestUtilities.Requests;
-public class RequestRegisterExpenseJsonBuilder
+public class RequestExpenseJsonBuilder
 {
     public static RequestExpenseJson Build()
     {
